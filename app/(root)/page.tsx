26f81e70979789cafb24e854edb669543abdbbb5
Apoorva <apshuk17@gmail.com>
2023-07-10 23:04:16 +0530

    Add authentication using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button>
-        Click Me
-      </Button>
+      <p>This is a protected route.</p>
+      <UserButton afterSignOutUrl="/"/>
     </div>
 
   )
 }
+
+export default SetupPage;
